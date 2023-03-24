572c54c0cb6bd3735cd772bf33d02b3974969171
sohan <gamalathsohan@gmail.com>
2023-03-24 12:15:39 +0530

    Section Component List is Implemented

--- src/pages/UserDashboard/ResultCalculation.js
+++ src/pages/UserDashboard/ResultCalculation.js
@@ -38,26 +38,33 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
   border: "none",
 }));*/
 
-function createData(description, equal, amount) {
-  return { description, equal, amount };
+function createData(description, amount) {
+  return { description, amount };
 }
 
 const ResultCalculation = (props) => {
   const [isResultUpdated, setIsResultUpdated] = useState(false);
   const { projectId } = props;
+  const [isProject, setIsProject] = useState(false);
+
   const [rows, setRows] = useState([
-    createData("Total Units", "=", ""),
-    createData("Usage Charge", "=", ""),
-    createData("Total Charge", "=", ""),
-    createData("Tax", "=", ""),
-    createData("Bill Amount", "=", ""),
+    createData("Total Units", ""),
+    createData("Usage Charge", ""),
+    createData("Total Charge", ""),
+    createData("Tax", ""),
+    createData("Bill Amount", ""),
   ]);
+
+  useEffect(() => {
+    setIsProject(props.isProject);
+  });
 
   const handleCalculation = async () => {
     const accessToken = localStorage.getItem("accessToken");
+    console.log(accessToken);
     try {
       const response = await axios.get(
-        `http://localhost:8080/play_ground/bill?projectId=${projectId}`,
+        `http://localhost:8080/playground/bill?projectId=${projectId}`,
         {
           headers: {
             Authorization: `Bearer ${accessToken}`,
@@ -68,11 +75,12 @@ const ResultCalculation = (props) => {
         console.log(response);
 
         const updatedRows = [
-          createData("Total Units", "=", response.data.data[0].totalUnits),
-          createData("Usage Charge", "=", response.data.data[0].usageCharge),
-          createData("Total Charge", "=", response.data.data[0].totalCharge),
-          createData("Tax", "=", response.data.data[0].levy),
-          createData("Bill Amount", "=", response.data.data[0].billAmount),
+          createData("Total Units", response.data.data[0].totalUnits),
+          createData("Usage Charge", response.data.data[0].usageCharge),
+          createData("Total Charge", response.data.data[0].totalCharge),
+          createData("Tax", response.data.data[0].levy),
+
+          createData("Bill Amount", response.data.data[0].billAmount),
         ];
 
         setRows(updatedRows);
@@ -86,85 +94,85 @@ const ResultCalculation = (props) => {
   return (
     <Box
       sx={{
-        mt: 3,
+        mt: 2,
 
         width: "100%",
         justifyContent: "space-evenly",
       }}
     >
-      <Box
-        p={3}
-        sx={{
-          mt: 5,
-          mb: 3,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          width: "100%",
-          justifyContent: "space-evenly",
-        }}
-      >
-        <Button
-          variant="contained"
-          color="success"
-          onClick={handleCalculation}
-          sx={{ width: "75%", justifyItems: "center" }}
+      {isProject && (
+        <Box
+          p={3}
+          sx={{
+            mt: 5,
+            mb: 3,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "100%",
+            justifyContent: "space-evenly",
+          }}
         >
-          Calculate
-        </Button>
+          <Button
+            variant="contained"
+            color="success"
+            onClick={handleCalculation}
+            sx={{ width: "60%", justifyItems: "center" }}
+          >
+            Calculate
+          </Button>
 
-        {isResultUpdated && (
-          <TableContainer sx={{ mt: 3 }}>
-            <Table
-              sx={{
-                minWidth: 500,
-                width: "60%",
-                mx: "auto",
-                borderColor: "#fff",
-                border: "none", // Add this line to remove the border
-              }}
-              aria-label="simple table"
-            >
-              <TableHead>
-                <TableRow>
-                  <StyledTableCell></StyledTableCell>
-                  <StyledTableCell align="right"></StyledTableCell>
-                  <StyledTableCell align="right"></StyledTableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map((row) => (
-                  <TableRow
-                    key={row.name}
-                    sx={{
-                      border: "none",
-                    }}
-                  >
-                    <StyledTableCell
-                      component="th"
-                      scope="row"
-                      sx={{ width: "40%" }}
-                    >
-                      {row.description}
-                    </StyledTableCell>
-                    <StyledTableCell align="right" sx={{ width: "10%" }}>
-                      {row.equal}
-                    </StyledTableCell>
-                    <StyledTableCell align="right">
-                      {row.description === "Bill Amount" ? (
-                        <span style={{ fontWeight: "bold" }}>{row.amount}</span>
-                      ) : (
-                        row.amount
-                      )}
-                    </StyledTableCell>
+          {isResultUpdated && (
+            <TableContainer sx={{ mt: 3 }}>
+              <Table
+                sx={{
+                  minWidth: 500,
+                  width: "60%",
+                  mx: "auto",
+                  borderColor: "#fff",
+                  border: "none", // Add this line to remove the border
+                }}
+                aria-label="simple table"
+              >
+                <TableHead>
+                  <TableRow>
+                    <StyledTableCell></StyledTableCell>
+                    <StyledTableCell align="right"></StyledTableCell>
                   </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        )}
-      </Box>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row) => (
+                    <TableRow
+                      key={row.name}
+                      sx={{
+                        border: "none",
+                      }}
+                    >
+                      <StyledTableCell
+                        component="th"
+                        scope="row"
+                        sx={{ width: "40%" }}
+                      >
+                        {row.description}
+                      </StyledTableCell>
+                      <StyledTableCell align="right">
+                        {row.description === "Bill Amount" ? (
+                          <span style={{ fontWeight: "bold" }}>
+                            {row.amount}
+                          </span>
+                        ) : (
+                          row.amount
+                        )}
+                      </StyledTableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          )}
+        </Box>
+      )}
     </Box>
   );
 };
